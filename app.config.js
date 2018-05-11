81fe47e4f18c8dbf5a6c160fccd1707cc8d7ea6b
Christopher Green <cg433n@email.arizona.edu>
2018-05-11 07:57:21 -0700

    Updates domain Refs #10387

--- app.config.js
+++ app.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  domain: 'http://exl-impl-primo.hosted.exlibrisgroup.com',
+  domain: 'https://arizona-primo.hosted.exlibrisgroup.com',
   institution: '01UA_INST',
   vid: '01UA'
 }
