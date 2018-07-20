92d371b53a15bc92078545e65bc076669dded5c1
Andy Osborne <cao89@email.arizona.edu>
2018-07-20 14:24:28 -0700

    Include node_modules in include and add babel-plugin-transform-runtime as dev dependency refs #10578

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     rules: [
       {
         test: /\.js?$/,
-        include: [path.resolve(__dirname, './src')],
+        include: [path.resolve(__dirname, './src'), path.resolve(__dirname, './node_modules')],
         loader: 'babel-loader',
         options: {
           presets: ['env', 'stage-0', 'react'],
